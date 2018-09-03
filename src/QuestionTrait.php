795e9c0f03a03f5e14586153b1e9da4798c8a644
moomnng <moomnng@qq.com>
2018-09-03 15:31:53 +0800

    新增接口

---
+++ src/QuestionTrait.php
@@ -0,0 +1,97 @@
+<?php
+namespace Lexiangla\Openapi;
+
+Trait QuestionTrait
+{
+    public function postQuestion($staff_id, $attributes, $options = [])
+    {
+        $document = [
+            'data' => [
+                'type' => 'question',
+                'attributes' => [
+                    'title' => $attributes['title'],
+                    'content' => $attributes['content'],
+                    'is_anonymous' => $attributes['is_anonymous'],
+                    'tags' => $attributes['tags'],
+                ]
+            ]
+        ];
+        foreach ($options as $k => $v) {
+            $document['data']['attributes'][$k] = $v;
+        }
+        return $this->forStaff($staff_id)->post('questions', $document);
+    }
+
+    public function patchQuestion($staff_id, $question_id, $attributes)
+    {
+        $document = [
+            'data' => [
+                'type' => 'question',
+            ]
+        ];
+        foreach ($attributes as $k => $v) {
+            $document['data']['attributes'][$k] = $v;
+        }
+        return $this->forStaff($staff_id)->patch('questions/' . $question_id, $document);
+    }
+
+    public function deleteQuestion($staff_id, $question_id)
+    {
+        return $this->forStaff($staff_id)->delete('questions/' . $question_id);
+    }
+
+    public function getQuestion($id, $request = [])
+    {
+        return $this->get('questions/' . $id, $request);
+    }
+
+    public function postQuestionAnswer($staff_id, $question_id, $attributes, $options = [])
+    {
+        $document = [
+            'data' => [
+                'type' => 'answer',
+                'attributes' => [
+                    'content' => $attributes['content'],
+                    'is_anonymous' => $attributes['is_anonymous'],
+                ]
+            ]
+        ];
+        foreach ($options as $k => $v) {
+            $document['data']['attributes'][$k] = $v;
+        }
+        return $this->forStaff($staff_id)->post('questions/' . $question_id . '/answers', $document);
+    }
+
+    public function patchQuestionAnswer($staff_id, $question_id, $answer_id, $attributes, $options = [])
+    {
+        $document = [
+            'data' => [
+                'type' => 'answer',
+                'attributes' => [
+                    'content' => $attributes['content'],
+                ]
+            ]
+        ];
+        return $this->forStaff($staff_id)->patch('questions/' . $question_id . '/answers/' . $answer_id, $document);
+    }
+
+    public function deleteQuestionAnswer($staff_id, $question_id, $answer_id)
+    {
+        return $this->forStaff($staff_id)->delete('questions/' . $question_id . '/answers/' . $answer_id);
+    }
+
+    public function postQuestionConcerns($question_id, $staff_id)
+    {
+        $document = [
+            'data'=> [
+                [
+                    'type' => 'staff',
+                    'id' => $staff_id,
+                ]
+            ]
+        ];
+
+        return $this->post('questions/' . $question_id . '/concerns', $document);
+    }
+
+}
---
+++ src/ThreadTrait.php
@@ -0,0 +1,82 @@
+<?php
+namespace Lexiangla\Openapi;
+
+Trait ThreadTrait
+{
+    public function postThread($staff_id, $attributes, $options = [])
+    {
+        $document = [
+            'data' => [
+                'type' => 'thread',
+                'attributes' => [
+                    'title' => $attributes['title'],
+                    'content' => $attributes['content'],
+                ],
+                'relationships' => [
+                    'category' => [
+                        'data' => [
+                            'type' => 'category',
+                            'id' => $attributes['category_id']
+                        ]
+                    ],
+                ]
+            ]
+        ];
+
+        foreach ($options as $k => $v) {
+            $document['data']['attributes'][$k] = $v;
+        }
+        return $this->forStaff($staff_id)->post('threads', $document);
+    }
+
+    public function getThread($id, $request = [])
+    {
+        return $this->get('threads/' . $id, $request);
+    }
+
+    public function deleteThread($staff_id, $thread_id)
+    {
+        return $this->forStaff($staff_id)->delete('threads/' . $thread_id);
+    }
+
+    public function postThreadPost($staff_id, $thread_id, $attributes, $options = [])
+    {
+        $document = [
+            'data' => [
+                'type' => 'post',
+                'attributes' => [
+                    'content' => $attributes['content'],
+                ]
+            ]
+        ];
+        if (isset($options['ref_id'])) {
+            $document['data']['relationships']['reference'] = [
+                'data' => [
+                    'type' => 'post',
+                    'id' => $options['ref_id']
+                ]
+            ];
+        }
+        foreach ($options as $k => $v) {
+            $document['data']['attributes'][$k] = $v;
+        }
+
+        return $this->forStaff($staff_id)->post('threads/' . $thread_id . '/posts', $document);
+    }
+
+    public function postThreadConcerns($thread_id, $staffs)
+    {
+        $document = [];
+        foreach ($staffs as $staff) {
+            $document['data'][] = [
+                'type' => 'staff',
+                'id' => $staff['id'],
+                'attributes' => [
+                    'created_at' => $staff['created_at']
+                ]
+            ];
+        }
+
+        return $this->forStaff('system-bot')->post('threads/' . $thread_id . '/concerns', $document);
+    }
+}
