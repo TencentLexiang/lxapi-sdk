6e69c1626beb474b7bba53a8b90a2d2830da8c31
morpheuslv <morpheuslv@tencent.com>
2019-10-18 10:25:12 +0800

    增加文件夹删除和更新方法

--- src/DocTrait.php
+++ src/DocTrait.php
@@ -191,4 +191,62 @@ Trait DocTrait
         }
         return $this->forStaff($staff_id)->post('directories', $document);
     }
+
+    public function deleteDirectory($staff_id, $directory_id)
+    {
+        return $this->forStaff($staff_id)->delete('directories/' . $directory_id);
+    }
+
+    public function patchDirectory($staff_id, $directory_id, $options)
+    {
+        $document = [
+            'data' => [
+                'type' => 'directory',
+            ]
+        ];
+
+        if (isset($options['title'])) {
+            $document['data']['attributes']['title'] = $options['title'];
+        }
+
+        if (isset($options['content'])) {
+            $document['data']['attributes']['content'] = $options['content'];
+        }
+
+        if (isset($options['privilege_type'])) {
+            $document['data']['attributes']['privilege_type'] = $options['privilege_type'];
+        }
+        if (isset($options['privilege'])) {
+            foreach ($options['privilege'] as $privilege) {
+                $document['data']['relationships']['privilege']['data'][] = $privilege;
+            }
+        }
+        if (isset($options['source'])) {
+            $document['data']['attributes']['source'] = $options['source'];
+        }
+        if (isset($options['reship_url'])) {
+            $document['data']['attributes']['reship_url'] = $options['reship_url'];
+        }
+        if (isset($options['category_id'])) {
+            $document['data']['relationships']['category']['data']['type'] = 'category';
+            $document['data']['relationships']['category']['data']['id'] = $options['category_id'];
+        }
+        if (isset($options['team_id'])) {
+            $document['data']['relationships']['team']['data']['type'] = 'team';
+            $document['data']['relationships']['team']['data']['id'] = $options['team_id'];
+        }
+        if (isset($options['directory_id'])) {
+            $document['data']['relationships']['directory']['data']['type'] = 'directory';
+            $document['data']['relationships']['directory']['data']['id'] = $options['directory_id'];
+        }
+        if (!empty($options['attachments'])) {
+            foreach ($options['attachments'] as $attachment_id) {
+                $document['data']['relationships']['attachments']['data'][] = [
+                    'type' => 'attachment',
+                    'id' => $attachment_id
+                ];
+            }
+        }
+        return $this->forStaff($staff_id)->patch('directories/' . $directory_id, $document);
+    }
 }
