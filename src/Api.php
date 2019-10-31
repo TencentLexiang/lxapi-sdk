4c42d4eb8b0871199a76d311554344278d5a5b5b
moomnng <moomnng@qq.com>
2019-10-31 18:04:16 +0800

    bugfix domain

--- src/Api.php
+++ src/Api.php
@@ -16,8 +16,8 @@ class Api
     use LikeTrait;
     use TeamTrait;
     use AttachmentTrait;
-    use VideoTrait;
-    protected $main_url = 'http://lxapi.swan.local/cgi-bin';
+
+    protected $main_url = 'https://lxapi.lexiangla.com/cgi-bin';
 
     protected $verson = 'v1';
 
