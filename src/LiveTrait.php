0866833d84f1c6e9cefd8d0169f9db3c6888f800
v_shlhengli(李圣) <v_shlhengli@tencent.com>
2021-06-01 15:04:12 +0800

    master

--- src/LiveTrait.php
+++ src/LiveTrait.php
@@ -46,6 +46,34 @@ Trait LiveTrait
         if (isset($options['enable_comment'])) {
             $document['data']['attributes']['enable_comment'] = $options['enable_comment'];
         }
+		if (isset($options['vertical'])) {
+            $document['data']['attributes']['vertical'] = $options['vertical'];
+		}
+        if (isset($options['auto_transfer_class'])) {
+            $document['data']['attributes']['auto_transfer_class'] = $options['auto_transfer_class'];
+		}
+		if (isset($options['live_type'])) {
+            $document['data']['attributes']['live_type'] = $options['live_type'];
+		}
+		if (isset($options['password'])) {
+            $document['data']['attributes']['password'] = $options['password'];
+		}
+		if (!empty($options['course_category_id'])){
+			foreach ($options['course_category_id']as $course_category_id){
+				$document['data']['relationships']['course_category']['data']=[
+					'type' => 'category',
+                    'id' => $course_category_id
+				];
+			}
+		}
+		if (!empty($options['class_category_id'])){
+			foreach ($options['class_category_id']as $class_category_id){
+				$document['data']['relationships']['class_category']['data']=[
+					'type' => 'category',
+                    'id' => $class_category_id
+				];
+			}
+		}
         if (!empty($options['manager_ids'])) {
             foreach ($options['manager_ids'] as $manager_id) {
                 $document['data']['relationships']['managers']['data'][] = [
@@ -59,7 +87,7 @@ Trait LiveTrait
                 $document['data']['relationships']['privilege']['data'][] = $privilege;
             }
         }
-
+		//return $document;
         return $this->forStaff($staff_id)->post('lives', $document);
     }
 
@@ -114,7 +142,7 @@ Trait LiveTrait
         }
         if (!empty($options['manager_ids'])) {
             foreach ($options['manager_ids'] as $manager_id) {
-                $document['data']['relationships']['managers']['data'][] = [
+                $document['data']['relationships']['managers']['data'] = [
                     'type' => 'staff',
                     'id' => $manager_id
                 ];
@@ -122,7 +150,7 @@ Trait LiveTrait
         }
         if (!empty($options['privilege'])) {
             foreach ($options['privilege'] as $privilege) {
-                $document['data']['relationships']['privilege']['data'][] = $privilege;
+                $document['data']['relationships']['privilege']['data']= $privilege;
             }
         }
 
