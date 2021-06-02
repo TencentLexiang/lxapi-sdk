e8861e73c376e946aac94cdd8ab0e42268e9ecba
v_shlhengli(李圣) <v_shlhengli@tencent.com>
2021-06-02 09:45:15 +0800

    master

--- src/LiveTrait.php
+++ src/LiveTrait.php
@@ -46,6 +46,30 @@ Trait LiveTrait
         if (isset($options['enable_comment'])) {
             $document['data']['attributes']['enable_comment'] = $options['enable_comment'];
         }
+        if (isset($options['vertical'])) {
+            $document['data']['attributes']['vertical'] = $options['vertical'];
+        }
+        if (isset($options['auto_transfer_class'])) {
+            $document['data']['attributes']['auto_transfer_class'] = $options['auto_transfer_class'];
+        }
+        if (isset($options['live_type'])) {
+            $document['data']['attributes']['live_type'] = $options['live_type'];
+        }
+        if (isset($options['password'])) {
+            $document['data']['attributes']['password'] = $options['password'];
+        }
+        if (!empty($options['course_category_id'])){
+            $document['data']['relationships']['course_category']['data']=[
+                'type' => 'category',
+                'id' => $options['course_category_id']
+            ];
+        }
+        if (!empty($options['class_category_id'])){
+            $document['data']['relationships']['class_category']['data']=[
+                'type' => 'category',
+                'id' => $options['class_category_id']
+            ];
+        }
         if (!empty($options['manager_ids'])) {
             foreach ($options['manager_ids'] as $manager_id) {
                 $document['data']['relationships']['managers']['data'][] = [
