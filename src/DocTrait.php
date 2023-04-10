fb9303ec12f3b0eb191b345eebe1408afdf9422a
ls-tx <84374159+ls-tx@users.noreply.github.com>
2023-04-10 12:02:00 +0800

    文档sdk支持水印和复制设置 (#20)
    * feat:文档更新
    
    * feat:文档更新
    
    ---------
    
    Co-authored-by: v_shlhengli <v_shlhengli@tencent.com>

--- src/DocTrait.php
+++ src/DocTrait.php
@@ -36,6 +36,12 @@ Trait DocTrait
         if (isset($options['enable_image_watermark'])) {
             $document['data']['attributes']['enable_image_watermark'] = $options['enable_image_watermark'];
         }
+        if (isset($options['enable_watermark'])) {
+            $document['data']['attributes']['enable_watermark'] = $options['enable_watermark'];
+        }
+        if (isset($options['enable_copy_limit'])) {
+            $document['data']['attributes']['enable_copy_limit'] = $options['enable_copy_limit'];
+        }
         if (isset($options['signature'])) {
             $document['data']['attributes']['signature'] = $options['signature'];
         }
@@ -181,6 +187,12 @@ Trait DocTrait
         }
         if (isset($options['enable_image_watermark'])) {
             $document['data']['attributes']['enable_image_watermark'] = $options['enable_image_watermark'];
+        }
+        if (isset($options['enable_watermark'])) {
+            $document['data']['attributes']['enable_watermark'] = $options['enable_watermark'];
+        }
+        if (isset($options['enable_copy_limit'])) {
+            $document['data']['attributes']['enable_copy_limit'] = $options['enable_copy_limit'];
         }
         if (isset($options['signature'])) {
             $document['data']['attributes']['signature'] = $options['signature'];
